b5fee5050f761f92b0b537abdc1241f4cd1c36be
Javier Hevia <javierhc22@gmail.com>
2020-07-09 17:30:37 -0600

    Components Part 2

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,17 +32,18 @@ function App() {
         </a>
       </header> */}
 
-       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
-      </div>
+        <div>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu  dishes={this.state.dishes}/>
+        </div>
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
