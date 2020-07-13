ada589e8160e2135a739f8c370a08faed07b24ae
Javier Hevia <javierhc22@gmail.com>
2020-07-13 11:31:37 -0600

    Single Page Applications Part 1

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -8,7 +8,12 @@ import Footer from './FooterComponent';
 
 import Home from './HomeComponent';
 
+import Contact from './ContactComponent';
+
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -18,6 +23,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS,
         selectedDish: null
     };
   }
@@ -31,7 +39,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-          <Home />
+          <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]
+          }/>
       );
     }
 
@@ -44,6 +55,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}  />} />
+              <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
         </Switch>
 
